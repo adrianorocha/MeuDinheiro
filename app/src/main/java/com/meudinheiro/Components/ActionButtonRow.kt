ff28a020b71a29cf318b17ce493c47588625f36a
adrianorocha <adrianosrocha@gmail.com>
2025-12-04 22:17:44 -0300

    Atualização.

--- app/src/main/java/com/meudinheiro/Components/ActionButtonRow.kt
+++ app/src/main/java/com/meudinheiro/Components/ActionButtonRow.kt
@@ -11,8 +11,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,6 +34,8 @@ import com.meudinheiro.R
 @Composable
 @Preview(showBackground = true)
 fun ActionButtonRow() {
+    val exibeFormulario = remember { mutableStateOf(false) }
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -37,20 +44,83 @@ fun ActionButtonRow() {
     ) {
         ActionButton(
             R.drawable.deposit,
-             "Depositar",
+            "Depositar",
             onClick = { /* Ação ao clicar no botão Adicionar */ }
         )
         ActionButton(
-             R.drawable.add,
-             "Adicionar",
-            onClick = { /* Ação ao clicar no botão Relatórios */ }
+            R.drawable.add,
+            "Adicionar",
+            onClick = { exibeFormulario.value= true }
         )
         ActionButton(
-             R.drawable.sim_chip,
-             "Configurações",
+            R.drawable.sim_chip,
+            "Configurações",
             onClick = { /* Ação ao clicar no botão Configurações */ }
         )
+        exibeFormulario(exibeFormulario.value)
     }
+}
+@Composable
+fun exibeFormulario(exibe: Boolean) {
+    if (exibe) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Text(text = "Novas Despesas",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+            var descricao by remember { mutableStateOf("") }
+            var valor by remember { mutableStateOf("") }
+            var data by remember { mutableStateOf("") }
+
+            TextField(
+                value = descricao,
+                onValueChange = { descricao = it },
+                label = { Text("Descrição") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+            )
+            TextField(
+                value = valor,
+                onValueChange = { valor = it },
+                label = { Text("Valor da Despesa") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+            )
+            TextField(
+                value = data,
+                onValueChange = { data = it },
+                label = { Text("Data") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Button(
+                    onClick = { }
+                ) {
+                    Text(text = "Cancelar")
+                }
+                Button(
+                    onClick = {  }
+                ) {
+                    Text(text = "Salvar")
+
+
+            }
+        }
+    }
+}
 }
 @Composable
 fun RowScope.ActionButton(icon: Int, text: String, onClick: () -> Unit) {
@@ -60,7 +130,7 @@ fun RowScope.ActionButton(icon: Int, text: String, onClick: () -> Unit) {
             .height(78.dp)
             .clip(RoundedCornerShape(10.dp))
             .background(color = LightGray)
-            .clickable{}
+            .clickable{onClick()}
             .padding(8.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
